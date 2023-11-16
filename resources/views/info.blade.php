9297f260cebe387f383bfc013d8d127ea44ceca9
AlessandroCingolani <alessandro.cingolani93@gmail.com>
2023-11-16 14:33:38 +0100

    [ init ] info page

---
+++ resources/views/info.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Info</title>
+</head>
+
+<body>
+    <header>
+        <ul>
+            <li><a href="/">Return Home page</a></li>
+            <li><a href="/about">Return About page</a></li>
+        </ul>
+    </header>
+    <main>
+        <h3>{{ $title }}</h3>
+        <p>{{ $text }}</p>
+    </main>
+</body>
+
+</html>
